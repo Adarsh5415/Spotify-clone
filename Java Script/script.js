82b0114a74a4c1c1df4d2300731fc19620457457
Adarsh5415 <136148151+Adarsh5415@users.noreply.github.com>
2024-07-15 16:43:42 +0530

    Update script.js

--- Java Script/script.js
+++ Java Script/script.js
@@ -88,7 +88,7 @@ const playMusic=(track,pause=false)=>{
 
 async function displayAlbums(){
 
-    let a = await fetch(`http://127.0.0.1:3000/songs`);
+    let a = await fetch(`https://github.com/Adarsh5415/spotify-clone/songs`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
@@ -102,7 +102,7 @@ async function displayAlbums(){
     if (e.href.includes("/songs")) {
         let folder = e.href.split("/").slice(-2)[0]
         //Get the meta dta of the folder
-        let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+        let a = await fetch(`https://github.com/Adarsh5415/spotify-clone/songs/${folder}/info.json`);
         let response = await a.json();
         console.log(response);
         cardContainer.innerHTML = cardContainer.innerHTML + `<div data-folder="${folder}"  class="card">
